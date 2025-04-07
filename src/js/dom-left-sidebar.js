603350f83d7591e0c8f2538199a2408a5f6e585d
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-04-07 09:33:01 -0300

    Fix: Enhanced sidebar positions, transitions, and sizes
    - Widths and font sizes that were declared in vw have been changed to
    rem.
    - The title of the new project warning is fixed — it now appears and
    disappears when the project sections' visibility is toggled.
    - Minor enhancements were also implemented for the add new button and
    its warnings.
    - The layout of the sidebar-collapse button is fixed: It no longer
    shifts when a project is added.

--- src/js/dom-left-sidebar.js
+++ src/js/dom-left-sidebar.js
@@ -1,138 +1,162 @@
-import '/src/css/left-sidebar.css';
-import { CurrentUser } from './index.js';
-import { warn } from 'vue';
+import "/src/css/left-sidebar.css";
 
-const LeftSidebar = ( () => {
-    const sidebarDiv = document.querySelector('#left-sidebar');
-    
+const LeftSidebar = (() => {
+  const sidebarDiv = document.querySelector("#left-sidebar");
 
-    const toggleVisibility = () => {
-        if (sidebarDiv.classList.contains('collapsed')){
-            sidebarDiv.classList.remove('collapsed');
-        } else {
-            sidebarDiv.classList.add('collapsed');
-        }
+  const toggleVisibility = () => {
+    if (sidebarDiv.classList.contains("collapsed")) {
+      sidebarDiv.classList.remove("collapsed");
+    } else {
+      sidebarDiv.classList.add("collapsed");
+    }
+  };
+
+  const _createProjectButton = (project) => {
+    const projectDiv = document.createElement("div");
+    projectDiv.classList.add("option", "project");
+    const projectP = document.createElement("p");
+    projectP.textContent = `# ${project.title}`;
+    projectDiv.appendChild(projectP);
+
+    return projectDiv;
+  };
+
+  const updateUserProjects = (user) => {
+    const projectsList = document.querySelector(".user-projects");
+    const addProjectBtn = document.querySelector(".add.project");
+    while (projectsList.firstChild !== addProjectBtn) {
+      projectsList.removeChild(projectsList.firstChild);
+    }
+    user.projects.forEach((project) => {
+      projectsList.insertBefore(_createProjectButton(project), addProjectBtn);
+    });
+  };
+
+  const _resetAddProjectsBtn = () => {
+    const addBtnDiv = document.querySelector(".add.project");
+    const addBtnP = document.querySelector(".add.project > p");
+    const addBtnImg = document.querySelector(".add.project > img");
+    const input = document.querySelector(".add.input");
+    input.value = "";
+    addBtnDiv.classList.remove("clicked");
+    addBtnP.classList.remove("hidden");
+    addBtnImg.classList.remove("hidden");
+    input.classList.add("hidden");
+  };
+
+  const toggleProjectsVisibility = () => {
+    const collapseBtn = document.querySelector(".projects-collapse");
+    const addProjectInput = document.querySelector(".add.project > input");
+    if (collapseBtn.classList.contains("collapsed")) {
+      collapseBtn.classList.remove("collapsed");
+    } else {
+      const existingWarning = document.querySelector(".project-warning");
+      collapseBtn.classList.add("collapsed");
+      if (existingWarning) {
+        existingWarning.remove();
+      }
+      setTimeout(() => _resetAddProjectsBtn(), 300); // wait for the fade-out animation
+    }
+  };
+
+  const _projectTitleWarning = (msg = "error", input) => {
+    const projectsList = document.querySelector(".user-projects");
+    const existingWarning = document.querySelector(".project-warning");
+    if (existingWarning) {
+      existingWarning.remove();
+    }
+    const warningP = document.createElement("p");
+    warningP.classList.add("project-warning");
+    warningP.textContent = String(msg);
+    projectsList.appendChild(warningP);
+    const controller = new AbortController();
+    const removeElement = (elem) => {
+      elem.remove();
+      controller.abort();
+      return;
     };
+    input.addEventListener("keydown", () => removeElement(warningP), {
+      signal: controller.signal,
+    });
+    input.addEventListener("blur", () => removeElement(warningP), {
+      signal: controller.signal,
+    });
+  };
 
-    const _createProjectButton = (project) => {
-        const projectDiv = document.createElement('div');
-        projectDiv.classList.add('option', 'project');
-        const projectP = document.createElement('p');
-        projectP.textContent = `# ${project.title}`;
-        projectDiv.appendChild(projectP);
-
-        return projectDiv;
-    }
-
-    const updateUserProjects = (user) => {
-        const projectsList = document.querySelector('.user-projects');
-        const addProjectBtn = document.querySelector('.add.project');
-        while (projectsList.firstChild !== addProjectBtn){
-            projectsList.removeChild(projectsList.firstChild);
-        };
-        user.projects.forEach(project => {
-            projectsList.insertBefore(_createProjectButton(project), addProjectBtn);
-        });
-    }
-
-    const toggleProjectsVisibility = () => {
-        const collapseBtn = document.querySelector('.projects-collapse');
-        if (collapseBtn.classList.contains('collapsed')){
-            collapseBtn.classList.remove('hidden');
-            collapseBtn.classList.remove('collapsed');
-        } else {
-            collapseBtn.classList.add('collapsed');
-            setTimeout(()=>{
-                collapseBtn.classList.add('hidden');
-            }, 300);
+  const _submitProject = (
+    input,
+    user,
+    controller,
+    key = null,
+    blur = false
+  ) => {
+    if ((key !== null && key.code === "Enter") || blur) {
+      if (input.value === "") {
+        _resetAddProjectsBtn();
+        return;
+      }
+      const titleEntered = user.validateProjectTitle(input.value);
+      if (titleEntered === null) {
+        _projectTitleWarning("Oops! Not a real name, buddy.", input);
+        setTimeout(() => {
+          input.focus();
+        }, 10);
+        return;
+      } else if (titleEntered === false) {
+        // invalid title
+        _projectTitleWarning("This name is already in use", input);
+        setTimeout(() => {
+          input.focus();
+        }, 1);
+        return;
+      } else {
+        // valid title
+        const success = user.addProject(titleEntered);
+        if (success) {
+          controller.abort();
+          updateUserProjects(user);
+          _resetAddProjectsBtn();
+          input.value = "";
+          return;
         }
+      }
     }
+  };
 
-    const _resetAddProjectsBtn = () => {
-        const addBtnDiv = document.querySelector('.add.project');
-        const addBtnP = document.querySelector('.add.project > p');
-        const addBtnImg = document.querySelector('.add.project > img');
-        const input = document.querySelector('.add.input');
-        addBtnDiv.classList.remove('clicked');
-        addBtnP.classList.remove('hidden');
-        addBtnImg.classList.remove('hidden');
-        input.classList.add('hidden');
-    }
+  const _listenForSubmit = (input, user) => {
+    const controller = new AbortController();
 
-    const _projectTitleWarning = (msg = 'error', input) => {
-        const projectsList = document.querySelector('.user-projects');
-        const existingWarning = document.querySelector('.project-warning');
-        if (existingWarning){
-            existingWarning.remove();
-        }
-        const warningP = document.createElement('p');
-        warningP.classList.add('project-warning');
-        warningP.textContent = String(msg);
-        projectsList.appendChild(warningP);
-        const controller = new AbortController();
-        const removeElement = (elem) => {
-            elem.remove();
-            controller.abort();
-            return;
-        }
-        input.addEventListener('keydown', () => removeElement(warningP), { signal : controller.signal });
-        input.addEventListener('blur', () => removeElement(warningP), { signal : controller.signal });
-        
-    }
+    input.addEventListener(
+      "keydown",
+      (key) => _submitProject(input, user, controller, key),
+      { signal: controller.signal }
+    );
+    input.addEventListener(
+      "blur",
+      () => _submitProject(input, user, controller, null, true),
+      { signal: controller.signal }
+    );
+  };
 
-    const _listenForSubmit = (input, user) => {
-        const controller = new AbortController();
+  const clickOnAddProject = (user) => {
+    const addBtnDiv = document.querySelector(".add.project");
+    const addBtnP = document.querySelector(".add.project > p");
+    const addBtnImg = document.querySelector(".add.project > img");
+    const input = document.querySelector(".add.input");
+    addBtnDiv.classList.add("clicked");
+    addBtnP.classList.add("hidden");
+    addBtnImg.classList.add("hidden");
+    input.classList.remove("hidden");
+    input.focus();
+    _listenForSubmit(input, user);
+  };
 
-        const submitProject = (key = null, blur = false) => {
-            if ((key !== null && key.code === 'Enter') || blur) {
-                if (input.value === ''){
-                    _resetAddProjectsBtn();
-                    return;
-                }
-                const titleEntered = user.validateProjectTitle(input.value);
-                if (titleEntered === null) {
-                    _projectTitleWarning('It is not a name friend', input);
-                    return;
-                } else if (titleEntered === false) { // invalid title
-                    _projectTitleWarning('The name already in use', input);
-                    return;
-                } else { // valid title
-                    const success = user.addProject(titleEntered);
-                    if (success) {
-                        controller.abort();
-                        updateUserProjects(user);
-                        _resetAddProjectsBtn();
-                        input.value = '';
-                        return;
-                    }
-                }
-            }
-        }
-        input.addEventListener('keydown', (key) => submitProject(key), { signal : controller.signal });
-        input.addEventListener('blur', () => submitProject(null, true), { signal : controller.signal });
-    }
-
-    const clickOnAddProject = (user) => {
-        const addBtnDiv = document.querySelector('.add.project');
-        const addBtnP = document.querySelector('.add.project > p');
-        const addBtnImg = document.querySelector('.add.project > img');
-        const input = document.querySelector('.add.input');
-        addBtnDiv.classList.add('clicked');
-        addBtnP.classList.add('hidden');
-        addBtnImg.classList.add('hidden');
-        input.classList.remove('hidden');
-        input.focus();
-        _listenForSubmit(input, user);
-    }
-
-
-    return { 
-        toggleVisibility,
-        updateUserProjects,
-        toggleProjectsVisibility,
-        clickOnAddProject,
-    };
-
-} )();
+  return {
+    toggleVisibility,
+    updateUserProjects,
+    toggleProjectsVisibility,
+    clickOnAddProject,
+  };
+})();
 
 export { LeftSidebar };
--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,37 @@
-import '/src/css/global.css';
-import { Task } from './tasks.js';
-import { Project } from './projects.js';
-import { MainContent } from './dom-main-content.js';
-import { LeftSidebar } from './dom-left-sidebar.js';
-import { User } from './users.js';
+import "/src/css/global.css";
+import { Task } from "./tasks.js";
+import { Project } from "./projects.js";
+import { MainContent } from "./dom-main-content.js";
+import { LeftSidebar } from "./dom-left-sidebar.js";
+import { User } from "./users.js";
 
-const CurrentUser = new User('Anonymous');
+const CurrentUser = new User("Anonymous");
 LeftSidebar.updateUserProjects(CurrentUser);
 const Content = new MainContent();
-    
+
 const handlePageClicks = (e) => {
-    const target = e.target.closest('[data-click]');
+  const target = e.target.closest("[data-click]");
 
-    if (target === null) { return };
+  if (target === null) {
+    return;
+  }
 
-    switch (target.dataset.click) {
-        case 'inbox':
-            Content.loadMainContent('inbox');
-            break;
-        case 'collapse-projects':
-            LeftSidebar.toggleProjectsVisibility();
-            break;
-        case 'collapse-sidebar':
-            LeftSidebar.toggleVisibility();
-            break;
-        case 'add-project':
-            LeftSidebar.clickOnAddProject(CurrentUser);
-            break;
-    }
-}
+  switch (target.dataset.click) {
+    case "inbox":
+      Content.loadMainContent("inbox");
+      break;
+    case "collapse-projects":
+      LeftSidebar.toggleProjectsVisibility();
+      break;
+    case "collapse-sidebar":
+      LeftSidebar.toggleVisibility();
+      break;
+    case "add-project":
+      LeftSidebar.clickOnAddProject(CurrentUser);
+      break;
+  }
+};
 
-document.body.addEventListener('click', handlePageClicks);
+document.body.addEventListener("click", handlePageClicks);
 
 export { CurrentUser };
