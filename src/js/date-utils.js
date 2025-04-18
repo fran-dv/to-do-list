81b35f5d6ab3c0d0bf1f9a8689ed3d4aab94441d
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-04-18 08:13:34 -0300

    feat: add method to format dates

--- src/js/date-utils.js
+++ src/js/date-utils.js
@@ -1,4 +1,4 @@
-import { getHours } from "date-fns";
+import { format, getHours } from "date-fns";
 
 export const DateUtils = (() => {
     const getDayMoment = () => {
@@ -9,7 +9,27 @@ export const DateUtils = (() => {
       return "evening";
     }
 
+    const _getCurrentYear = () => {
+      return new Date().getFullYear();
+    }
+
+    const getFormattedDate = (date) => {
+      if (!(date instanceof Date)){
+        console.error("Please pass a valid Date");
+        return;
+      }
+
+      const currentYear = _getCurrentYear();
+
+      if (date.getFullYear() > currentYear) {
+        return format(date, "MMM d yyyy");
+      } else {
+        return format(date, "MMM d");
+      }
+    }
+
     return {
         getDayMoment,
+        getFormattedDate,
     }
 })();
